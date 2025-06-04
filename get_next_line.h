099e2137c9fda7a09a3b02c0e1a7d2fdb9437093
Umar <umarsajidm989@gmail.com>
2025-06-04 20:42:28 +0300

    starting working on it

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: musajid <musajid@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/04 20:31:44 by musajid           #+#    #+#             */
+/*   Updated: 2025/06/04 20:37:55 by musajid          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+#define BUFFER_SIZE = 100
+
+char	*get_next_line(int fd);
+
+
+#endif
