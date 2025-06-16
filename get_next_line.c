e7b900dff9b7b81cc19a95f8d44b23d7e7278c1c
Umar <umarsajidm989@gmail.com>
2025-06-16 16:11:29 +0300

    still working

--- get_next_line.c
+++ get_next_line.c
@@ -5,6 +5,7 @@
 # include <stdio.h>
 # include <fcntl.h>
 
+
 // // char	*get_next_line(int fd);
 
 int putstr(char *str)
@@ -20,11 +21,66 @@ int putstr(char *str)
    // write(1, "\n", 1);
     return (i);
 }
+size_t	ft_strlen(const char *c)
+{
+	int	i;
 
-// // char	*get_next_line(int fd)
-// // {
-	
-// // }
+	i = 0;
+	if (!c)
+		return (0);
+	if (c[i] != '\0')
+	{
+		while (c[i] != '\0')
+			++i;
+		return (i);
+	}
+	else
+		return (0);
+}
+
+char	*ft_strdup(const char *s)
+{
+	size_t	i;
+	char	*s_dup;
+
+	s_dup = (char *)malloc(ft_strlen(s) + 1);
+	i = 0;
+	if (!s_dup)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		s_dup[i] = s[i];
+		i++;
+	}
+	s_dup[i] = '\0';
+	return (s_dup);
+}
+
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	if (len >= ft_strlen(s + start))
+		len = ft_strlen(s + start);
+	sub = (char *)malloc(len + 1);
+	if (!sub)
+		return (NULL);
+	while (i < len)
+	{
+		sub[i] = s[start + i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
+
 
 
 int main()
@@ -32,37 +88,32 @@ int main()
     char buffer[900];
     int i = 0;
     int byte_read;
-    char line[500];
+    char *string;
     int j = 0;
- 
-    
+
+
     int fd = open("text.txt", O_RDONLY);
     if (fd == -1)
         return (-1);
 
     byte_read = read(fd, buffer, 500);
     buffer[byte_read] = '\0';
-    while (buffer[i] && byte_read > 0)
-    {
-        if (buffer[i] == '\n')
-        {
-            while (j < i)
-                {
-                line[j] = buffer[j];
-                j++;
-                }
-            line[j] = '\0';
-            break;
-        }
-        i++;            
-    }
 
-    close(fd);
-    
-
-    printf("%s", line);
-    return 0;
-
+	while (buffer[i])
+{
+	if (buffer[i] == '.')
+	{
+		string = ft_substr(buffer, j, i - j + 1);
+		putstr(string);
+		write(1, "\n", 1);
+		free(string);
+		j = i + 1;
+	}
+	i++;
 }
+    close(fd);
+    return 0;
+}
+
 
 
